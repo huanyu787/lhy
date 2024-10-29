feafb029cb1d585ea3db4e4c8138c5c1491aa3a9
小麥 <a302854888@gmail.com>
2024-10-29 15:06:19 +0800

    Finish read file

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,56 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<bool> primeTable = [];
         public MainWindow()
         {
             InitializeComponent();
+            buildPrimeTable();
+        }
+
+        private void buildPrimeTable()
+        {
+            for (int i = 1; i <= 1e7; i++)
+            {
+                primeTable.Add(true);
+            }
+            primeTable[1] = false;
+
+            for (int i = 2; i < 1e7; i++)
+            {
+                if (!primeTable[i])
+                {
+                    continue;
+                }
+                for (int j = 2; i * j < 1e7; j++)
+                {
+                    primeTable[i * j] = false;
+                }
+            }
+        }
+
+        private void CalculatePrimes(object sender, RoutedEventArgs e)
+        {
+            int number = 0;
+            if (!int.TryParse(MyTextBox.Text, out number))
+            {
+                MyTextBlock.Text = "請輸入數字";
+                return;
+            }
+            if (number < 2)
+            {
+                MyTextBlock.Text = "請輸入大於等於2的數字";
+                return;
+            }
+
+            MyTextBlock.Text = "小於等於 " + number + " 的質數有:\n";
+            for (int i = 2; i <= number; i++)
+            {
+                if (primeTable[i])
+                {
+                    MyTextBlock.Text += i + " ";
+                }
+            }
         }
     }
 }
